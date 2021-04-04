8ecc3820ac9a0fb76a4da40ee5aa0e5e66b30e6e
Zemke <florian@zemke.io>
2021-04-04 17:35:06 +0200

    Check env port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./api/server.js');
 
-const port = process.argv[2] || 7171;
+const port = process.argv[2] || process.env.PORT || 7171;
 console.log("port is gonna be " + port);
 server.listen(port);
 
