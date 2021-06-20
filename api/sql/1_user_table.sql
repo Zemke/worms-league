ea4ab28d22dd921ad37abd9df5211b8ab73a15b2
Zemke <florian@zemke.io>
2021-06-20 12:10:43 +0200

    Add created and modified to user

--- api/sql/1_user_table.sql
+++ api/sql/1_user_table.sql
@@ -2,7 +2,9 @@ create table "user" (
   id serial primary key,
   username text not null,
   email text not null,
-  password text not null
+  password text not null,
+  created timestamp not null default now(),
+  modified timestamp not null default now()
 );
 
 create unique index uidx_user_username on "user" (username);
