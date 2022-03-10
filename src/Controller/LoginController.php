e91526a31f398bcc9f368405e24c274cebfc5d65
Zemke <florian@zemke.io>
2022-03-10 15:00:24 +0100

    Sign in

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,14 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+use App\Form\LoginFormType;
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    public function index(Request $request, AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('login/index.html.twig', [
+        $form = $this->createForm(LoginFormType::class, new User());
+        $error = $authenticationUtils->getLastAuthenticationError();
+        dump($error);
+        return $this->renderForm('login/index.html.twig', [
             'controller_name' => 'LoginController',
+            'form' => $form,
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error' => $error,
         ]);
     }
 }
