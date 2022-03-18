d110b0588d725793f0e2dbdcc040899082919872
Zemke <florian@zemke.io>
2022-03-18 16:02:19 +0100

    List opponents

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
+use App\Repository\UserRepository;
 
 class GameController extends AbstractController
 {
@@ -17,10 +20,22 @@ class GameController extends AbstractController
     }
 
     #[Route('/report', name: 'app_report')]
-    public function report(): Response
+    public function report(UserRepository $users,
+                           EntityManagerInterface $em,
+                           Security $security): Response
     {
+        $opponents = $em->createQueryBuilder()
+            ->select('u')
+            ->from('App:User', 'u')
+            ->where('u.id <> :authUserId')
+            ->orderBy('u.username', 'ASC')
+            ->getQuery()
+            ->setParameter('authUserId', $security->getUser()->getId())
+            ->getResult();
+        dump($opponents);
         return $this->render('game/report.html.twig', [
             'controller_name' => 'GameController',
+            'opponents' => $opponents,
         ]);
     }
 }
