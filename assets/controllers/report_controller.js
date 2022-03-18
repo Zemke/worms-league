8c4d00292a88b8bf8a1a2147aff5714a5aba23a9
Zemke <florian@zemke.io>
2022-03-18 12:50:17 +0100

    Show files right away

--- assets/controllers/report_controller.js
+++ assets/controllers/report_controller.js
@@ -2,19 +2,19 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
 
-    static targets = [ 'files', ];
+    static targets = [ 'files', 'fileInput', ];
 
     connect() {
+        this.listFiles();
     }
 
     greet() {
         console.log('called');
     }
 
-    onFileChange(e) {
-        const names = Array.from(e.target.files).map(f => f.name);
+    listFiles() {
+        const names = Array.from(this.fileInputTarget.files).map(f => f.name);
         console.log(names);
-        console.log(this.filesTarget);
         this.filesTarget.innerHTML = '';
         names.map(n => {
             const li = document.createElement('li');
