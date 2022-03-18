4af03272c69f336cecdcf5e3112eaa2f24e7f692
Zemke <florian@zemke.io>
2022-03-18 17:37:14 +0100

    make:entity for Game and Season

---
+++ src/Entity/Game.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GameRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GameRepository::class)]
+class Game
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\JoinColumn]
+    #[ORM\ManyToOne(targetEntity: "User")]
+    private $home;
+
+    #[ORM\JoinColumn]
+    #[ORM\ManyToOne(targetEntity: "User")]
+    private $away;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $scoreHome;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $scoreAway;
+
+    #[ORM\Column(type: 'datetime')]
+    private $created;
+
+    #[ORM\Column(type: 'datetime')]
+    private $modified;
+
+    #[ORM\JoinColumn]
+    #[ORM\ManyToOne(targetEntity: "User")]
+    private $reporter;
+
+    #[ORM\JoinColumn]
+    #[ORM\ManyToOne(targetEntity: "Season")]
+    private $season;
+
+    #[ORM\Column(type: 'boolean', options: ["default" => false])]
+    private $voided;
+
+    public function __construct() {
+        $this->voided = false;
+    }
+
+    public function get(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getHome(): ?int
+    {
+        return $this->home;
+    }
+
+    public function setHome(int $home): self
+    {
+        $this->home = $home;
+
+        return $this;
+    }
+
+    public function getAway(): ?int
+    {
+        return $this->away;
+    }
+
+    public function setAway(int $away): self
+    {
+        $this->away = $away;
+
+        return $this;
+    }
+
+    public function getScoreHome(): ?int
+    {
+        return $this->scoreHome;
+    }
+
+    public function setScoreHome(?int $scoreHome): self
+    {
+        $this->scoreHome = $scoreHome;
+
+        return $this;
+    }
+
+    public function getScoreAway(): ?int
+    {
+        return $this->scoreAway;
+    }
+
+    public function setScoreAway(?int $scoreAway): self
+    {
+        $this->scoreAway = $scoreAway;
+
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTimeInterface
+    {
+        return $this->created;
+    }
+
+    public function setCreated(\DateTimeInterface $created): self
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    public function getModified(): ?\DateTimeInterface
+    {
+        return $this->modified;
+    }
+
+    public function setModified(\DateTimeInterface $modified): self
+    {
+        $this->modified = $modified;
+
+        return $this;
+    }
+
+    public function getReporter(): ?int
+    {
+        return $this->reporter;
+    }
+
+    public function setReporter(int $reporter): self
+    {
+        $this->reporter = $reporter;
+
+        return $this;
+    }
+
+    public function getSeason(): ?int
+    {
+        return $this->season;
+    }
+
+    public function setSeason(int $season): self
+    {
+        $this->season = $season;
+
+        return $this;
+    }
+
+    public function getVoided(): ?bool
+    {
+        return $this->voided;
+    }
+
+    public function setVoided(bool $voided): self
+    {
+        $this->voided = $voided;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Season.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SeasonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SeasonRepository::class)]
+class Season
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'datetime')]
+    private $created;
+
+    #[ORM\Column(type: 'datetime')]
+    private $modified;
+
+    #[ORM\Column(type: 'datetime')]
+    private $start;
+
+    #[ORM\Column(type: 'datetime')]
+    private $ending;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreated(): ?\DateTimeInterface
+    {
+        return $this->created;
+    }
+
+    public function setCreated(\DateTimeInterface $created): self
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    public function getModified(): ?\DateTimeInterface
+    {
+        return $this->modified;
+    }
+
+    public function setModified(\DateTimeInterface $modified): self
+    {
+        $this->modified = $modified;
+
+        return $this;
+    }
+
+    public function getStart(): ?\DateTimeInterface
+    {
+        return $this->start;
+    }
+
+    public function setStart(\DateTimeInterface $start): self
+    {
+        $this->start = $start;
+
+        return $this;
+    }
+
+    public function getEnding(): ?\DateTimeInterface
+    {
+        return $this->ending;
+    }
+
+    public function setEnding(\DateTimeInterface $ending): self
+    {
+        $this->ending = $ending;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
