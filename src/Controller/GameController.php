2cc81331c4b6872d8771d91d525b87247826302f
Zemke <florian@zemke.io>
2022-03-18 10:45:17 +0100

    Report games page/controller

---
+++ src/Controller/GameController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GameController extends AbstractController
+{
+    #[Route('/game', name: 'app_game')]
+    public function index(): Response
+    {
+        return $this->render('game/index.html.twig', [
+            'controller_name' => 'GameController',
+        ]);
+    }
+
+    #[Route('/report', name: 'app_game')]
+    public function report(): Response
+    {
+        return $this->render('game/report.html.twig', [
+            'controller_name' => 'GameController',
+        ]);
+    }
+}
