f5bc72f04fbf6923fce55da76e02f35ea80ec5aa
Zemke <florian@zemke.io>
2022-04-06 18:51:45 +0200

    Perhaps unused method

--- src/Service/RankingService.php
+++ src/Service/RankingService.php
@@ -79,23 +79,5 @@ class RankingService
         }
         $this->em->flush();
     }
-
-    public function report(Game $game): Game
-    {
-        $winner = $this->rankingRepo->findOneOrCreate($game->winner(), $game->getSeason());
-        $loser = $this->rankingRepo->findOneOrCreate($game->loser(), $game->getSeason());
-        if ($game->draw()) {
-            $winner->plusPoints(1);
-            $loser->plusPoints(1);
-            $this->em->persist($winner);
-            $this->em->persist($loser);
-        } else {
-            $winner->plusPoints(3);
-            $this->em->persist($winner);
-        }
-        $this->em->persist($game);
-        $this->em->flush();
-        return $game;
-    }
 }
 
