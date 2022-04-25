4566344d47368f0c127dc0b20954da19a9f1eef6
Zemke <florian@zemke.io>
2022-04-25 17:06:44 +0200

    Bootstrap Grid to the rescue

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('bootstrap', './node_modules/bootstrap/dist/css/bootstrap-grid.css')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
