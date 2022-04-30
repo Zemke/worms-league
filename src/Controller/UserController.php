3403ec719ad9fb806baf9bde85181317681f08c4
Zemke <florian@zemke.io>
2022-04-30 15:51:15 +0200

    List opponent games

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -20,7 +20,21 @@ class UserController extends AbstractController
         } else {
             $user = $userRepo->findOneByUsername($usernameOrId);
         }
-        $games = $gameRepo->findOfUser($user);
+
+        $games = array_reduce($gameRepo->findOfUser($user), function ($acc, $g) use ($user) {
+            if (!$g->fullyProcessed()) {
+                return $acc;
+            }
+            $opp = $g->opponent($user);
+            $idx = array_search($opp, array_column($acc, 'opp'));
+            if ($idx === false) {
+                $acc[] = ['opp' => $opp, 'won' => $g->scoreOf($user), 'lost' => $g->scoreOf($opp)];
+            } else {
+                $acc[$idx]['won'] += $g->scoreOf($user);
+                $acc[$idx]['lost'] += $g->scoreOf($opp);
+            }
+            return $acc;
+        }, []);
         return $this->render('user/view.html.twig', [
             'user' => $user,
             'games' => $games,
