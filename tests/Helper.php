e2c4d8f5454cddf44ff32f55a10c9643dce1d83d
Zemke <florian@zemke.io>
2022-05-01 22:43:46 +0200

    Allow for builder

--- tests/Helper.php
+++ tests/Helper.php
@@ -5,12 +5,13 @@ namespace App\Tests;
 class Helper
 {
     // TODO migrate all mocks to this solution of setting the id
-    public static function setId(object $entity, int $value): void
+    public static function setId(object $entity, int $value): object
     {
         $class = new \ReflectionClass($entity);
         $property = $class->getProperty('id');
         $property->setAccessible(true);
         $property->setValue($entity, $value);
+        return $entity;
     }
 }
 
