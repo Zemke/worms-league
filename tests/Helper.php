c88d78fe1a6a9c0c62cca72cbeab3141ca45262c
Zemke <florian@zemke.io>
2022-05-01 21:14:57 +0200

    Helper for setting private ID

---
+++ tests/Helper.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests;
+
+class Helper
+{
+    // TODO migrate all mocks to this solution of setting the id
+    public static function setId(object $entity, int $value): void
+    {
+        $class = new \ReflectionClass($entity);
+        $property = $class->getProperty('id');
+        $property->setAccessible(true);
+        $property->setValue($entity, $value);
+    }
+}
+
