150ffe5713600f173336983a7e32f67f58b0e7b2
Zemke <florian@zemke.io>
2022-05-06 22:29:57 +0200

    Work on arrays

--- src/Twig/PadFilterExtension.php
+++ src/Twig/PadFilterExtension.php
@@ -15,9 +15,11 @@ class PadFilterExtension extends AbstractExtension
         ];
     }
 
-    public function padFn(string $v, \Traversable $path, string $node): string
+    public function padFn(string $v, \Traversable|array $path, string $node): string
     {
-        $vv = array_map(fn($v) => $v->$node(),  iterator_to_array($path));
+        $vv = array_map(
+            fn($v) => is_array($v) ? $v[$node] : $v->$node(),
+            is_array($path) ? $path : iterator_to_array($path));
         $mx_v = array_reduce($vv, fn($acc, $v) => max($acc, strlen($v)), 0);
         return str_repeat('&nbsp;', $mx_v - strlen($v)) . $v;
     }
