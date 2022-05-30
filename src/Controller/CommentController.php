5aa5d2fde9731c3100f133e2521a41a1ff3a6ee1
Zemke <florian@zemke.io>
2022-05-30 14:47:57 +0200

    CSRF violation for commenting

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
@@ -20,9 +21,12 @@ class CommentController extends AbstractController
                            int $gameId,
                            Security $security,
                            GameRepository $gameRepo,
-                           CommentRepository $commentRepo): RedirectResponse
+                           CommentRepository $commentRepo): Response
     {
         $game = $gameRepo->find($gameId);
+        if (!$this->isCsrfTokenValid('add-comment', $request->request->get('token'))) {
+            return new Response('', 403);
+        }
         if (is_null($game)) {
             throw $this->createNotFoundException("Game {$gameId} does not exist.");
         }
