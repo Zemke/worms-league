6a43e0a4d0623261465396e649bb618a01709c5f
Zemke <florian@zemke.io>
2022-08-07 14:17:25 +0200

    Form submit

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -4,13 +4,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MessageController extends AbstractController
 {
     #[Route('/message', name: 'app_message', methods: ['POST']) ]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('message/index.html.twig');
+        dump($request);
+        return $this->render('message/index.html.twig', [
+            'controller_name' => 'power to manipulate',
+        ]);
     }
 }
