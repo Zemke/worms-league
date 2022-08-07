cce6f8907d645e8314c47109e5364a8157da8ea8
Zemke <florian@zemke.io>
2022-08-07 14:08:06 +0200

    Suggestion styling, inputs

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\SeasonRepository;
+use App\Repository\UserRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/')]
-    public function index(SeasonRepository $seasonRepo): Response
+    public function index(SeasonRepository $seasonRepo,
+                          UserRepository $userRepo): Response
     {
         $var['season'] = $seasonRepo->findActive();
+        $var['users'] = $userRepo->findAll();
         return $this->render('home/index.html.twig', $var);
     }
 }
