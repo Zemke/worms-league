b4000f5cff35e6e8f01d4dfc6cc4c65693b226e3
Zemke <florian@zemke.io>
2022-09-27 22:12:23 +0200

    Reduce unused

--- src/Controller/LadderController.php
+++ src/Controller/LadderController.php
@@ -12,12 +12,8 @@ use App\Repository\SeasonRepository;
 class LadderController extends AbstractController
 {
     #[Route('/ladder', name: 'app_ladder')]
-    public function index(Request $request,
-                          RankingRepository $rankingRepo,
-                          SeasonRepository $seasonRepo): Response
+    public function index(Request $request): Response
     {
-        $seasonId = $request->query->getInt('season', -1);
-        $season = $seasonId === -1 ? $seasonRepo->findActive() : $seasonRepo->find($seasonId);
         return $this->render('ladder/index.html.twig', [
             'id' => $request->query->getInt('season', -1),
         ]);
