fb6ab749f27834c56277a2e76f2b879515df53e2
Zemke <florian@zemke.io>
2022-10-11 16:12:10 +0200

    Processing playoff games

--- src/MessageHandler/SendReplayMessageHandler.php
+++ src/MessageHandler/SendReplayMessageHandler.php
@@ -64,18 +64,65 @@ final class SendReplayMessageHandler implements MessageHandlerInterface
         } else {
             $this->logger->info("Replay {$replay->getId()} already has a map.");
         }
-        if ($replay->getGame()->fullyProcessed()) {
-            $this->gameRepo->add($replay->getGame()->score(), true);
-            if (!$replay->getGame()->getRanked()) {
-                $this->bus->dispatch(new RankingCalcMessage($replay->getGame()->getId()));
-                try {
-                    $msg = $replay->getGame()->asText()
-                        . ' https://wl.zemke.io/matches/' . $replay->getGame()->getId();
-                    $this->chatter->send(new ChatMessage($msg));
-                } catch (\Throwable $e) {
-                    $this->logger->critical($e->getMessage(), ['exception' => $e]);
+        if (!$replay->getGame()->fullyProcessed()) {
+            return;
+        }
+        $replay->getGame()->score();
+        if ($replay->getGame()->isPlayoff() && is_null($replay->getGame()->winner())) {
+            throw new UnrecoverableMessageHandlingException(
+                "{$replay->getGame()->asText()} is a playoff game and mustn't be drawn.");
+        }
+        $this->gameRepo->add($replay->getGame(), true);
+        if ($replay->getGame()->isPlayoff()) {
+            // playoff
+            $finalStep = log(array_reduce(
+               $playoffRepo->findForPlayoffs($replay->getGame()->getSeason(),
+                fn ($acc, $g) => $acc + ($g->getPlayoff()->getStep() === 1),
+                0)), 2) + 1;
+            $winner = $replay->getGame()->winner();
+            if ($finalStep - 1 === $replay->getGame()->getPlayoff()->getStep()) {
+                // semifinal
+                $advUsers = [$loser, $winner];
+                for ($i = 0; $i <= 1; $i++) {
+                    $advGame = (new Game())
+                        ->setPlayoff((new Playoff())
+                            ->setSpot(1)
+                            ->setStep($finalStep + $i));
+                    if ($replay->getGame()->getPlayoff()->getSpot() % 2 !== 0) {
+                        $advGame->setHome($advUsers[$i]);
+                    } else {
+                        $advGame->setAway($advUsers[$i]);
+                    }
+                    $this->gameRepo->add($advGame, $i === 1);
                 }
+            } else if ($replay->getGame()->getPlayoff()->getStep() < $finalStep) {
+                // pre-semifinal
+                $advGame = (new Game())
+                    ->setPlayoff((new Playoff())
+                        ->setSpot(ceil($replay->getGame()->getPlayoff()->getStep() / 2))
+                        ->setStep($replay->getGame()->getPlayoff()->getStep() + 1));
+                if ($replay->getGame()->getPlayoff()->getSpot() % 2 !== 0) {
+                    $advGame->setHome($winner);
+                } else {
+                    $advGame->setAway($winner);
+                }
+                $this->gameRepo->add($advGame, true);
             }
+            $this->chat($replay->getGame());
+        } else if (!$replay->getGame()->getRanked()) {
+            // ladder
+            $this->bus->dispatch(new RankingCalcMessage($replay->getGame()->getId()));
+            $this->chat($replay->getGame());
+        }
+    }
+
+    private function chat(Game $game): void
+    {
+        try {
+            $this->chatter->send(new ChatMessage(
+                $game->asText() . ' https://wl.zemke.io/matches/' . $game->getId()));
+        } catch (\Throwable $e) {
+            $this->logger->critical($e->getMessage(), ['exception' => $e]);
         }
     }
 }
