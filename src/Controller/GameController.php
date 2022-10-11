04d639e35ed1958fc048db53176243ae431f9aba
Zemke <florian@zemke.io>
2022-10-11 10:18:27 +0200

    Show playoff opponents

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -14,6 +14,7 @@ use Symfony\Component\Mime\Email;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use App\Repository\PlayoffRepository;
 use App\Repository\UserRepository;
 use App\Repository\SeasonRepository;
 use App\Entity\Game;
@@ -27,6 +28,7 @@ class GameController extends AbstractController
     #[Route('/report', name: 'app_report', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_USER')]
     public function report(Request $request,
+                           PlayoffRepository $playoffRepo,
                            UserRepository $users,
                            SeasonRepository $seasons,
                            EntityManagerInterface $em,
@@ -38,60 +40,72 @@ class GameController extends AbstractController
 
         $user = $this->getUser();
         $var = [ 'season' => $seasons->findOneBy(['active' => true]) ];
+        if ($var['season']->current()) {
+            $var['opponents'] = $em->createQueryBuilder()
+                ->select('u')
+                ->from('App:User', 'u')
+                ->where('u.id <> :authUserId')
+                ->orderBy('u.username', 'ASC')
+                ->getQuery()
+                ->setParameter('authUserId', $user->getId())
+                ->getResult();
+        } else {
+            $var['opponents'] = array_reduce($playoffRepo->findForPlayoffs($var['season']), function ($acc, $g) use ($user) {
+                if ($g->isHomeOrAway($user) && !$g->played()) {
+                    $acc[] = $g->opponent($user);
+                }
+                return $acc;
+            }, []);
+        }
+        if (empty($var['opponents'])) {
+            $this->addFlash('info', 'There are no opponents for you.');
+        }
         if (!isset($var['season'])) {
             $this->addFlash('error', 'There\'s currently no season.');
         } else if ($request->getMethod() === 'POST') {
-            if (!$var['season']->current()) {
-                $this->addFlash('error', 'The season has ended.');
-            }
             if (!$this->isCsrfTokenValid('report', $request->request->get('token'))) {
                 return new Response('', 403);
             }
-            $game = (new Game())
-                ->setReporter($user)
-                ->setHome($user)
-                ->setAway($users->find($request->request->all()['opponent']))
-                ->setSeason($var['season']);
-            foreach ($request->files->all('replays') as $file) {
-                $game->addReplay((new Replay())->setFile($file));
-            }
-            if (count($err = $validator->validate($game)) > 0) {
-                foreach ($err as $r) {
-                    $this->addFlash('error', $r->getMessage());
-                }
+            $opp = $users->find($request->request->all()['opponent']);
+            if (is_null($opp) || !in_array($opp->getId(), array_map(fn($u) => $u->getId(), $var['opponents']))) {
+                $this->addFlash('error', 'Invalid opponent.');
             } else {
-                $em->persist($game);
-                $em->flush();
-                foreach ($game->getReplays() as $replay) {
-                    $bus->dispatch(new SendReplayMessage($replay->getId()));
+                $game = (new Game())
+                    ->setReporter($user)
+                    ->setHome($user)
+                    ->setAway($opp)
+                    ->setSeason($var['season']);
+                foreach ($request->files->all('replays') as $file) {
+                    $game->addReplay((new Replay())->setFile($file));
                 }
-                try {
-                    $email = (new Email())
-                        ->to('florian@zemke.io')
-                        ->subject('WL REP: ' . $game->getId())
-                        ->text('https://wl.zemke.io/matches/' . $game->getId());
-                    $mailer->send($email);
-                } catch (\Throwable $e) {
-                    $logger->critical($e->getMessage(), ['exception' => $e]);
+                if (count($err = $validator->validate($game)) > 0) {
+                    foreach ($err as $r) {
+                        $this->addFlash('error', $r->getMessage());
+                    }
+                } else {
+                    $em->persist($game);
+                    $em->flush();
+                    foreach ($game->getReplays() as $replay) {
+                        $bus->dispatch(new SendReplayMessage($replay->getId()));
+                    }
+                    try {
+                        $email = (new Email())
+                            ->to('florian@zemke.io')
+                            ->subject('WL REP: ' . $game->getId())
+                            ->text('https://wl.zemke.io/matches/' . $game->getId());
+                        $mailer->send($email);
+                    } catch (\Throwable $e) {
+                        $logger->critical($e->getMessage(), ['exception' => $e]);
+                    }
+                    $this->addFlash('success', 'Your game has been reported and is being processed.');
+                    return $this->redirectToRoute(
+                        'app_match_view',
+                        ['gameId' => $game->getId()]
+                    );
                 }
-                $this->addFlash('success', 'Your game has been reported and is being processed.');
-                return $this->redirectToRoute(
-                    'app_match_view',
-                    ['gameId' => $game->getId()]
-                );
             }
-        }
-        $var['opponents'] = $em->createQueryBuilder()
-            ->select('u')
-            ->from('App:User', 'u')
-            ->where('u.id <> :authUserId')
-            ->orderBy('u.username', 'ASC')
-            ->getQuery()
-            ->setParameter('authUserId', $user->getId())
-            ->getResult();
-        if (empty($var['opponents'])) {
-            $this->addFlash('info', 'There are no opponents.');
         }
         return $this->render('game/report.html.twig', $var);
     }
 }
+
